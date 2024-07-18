365471186750d91d9c25ccbdb28fb26ccf020655
Rafael <rafa.505009@gmail.com>
2024-07-18 18:07:48 -0300

    arquivo real

---
+++ VitalityWeb/src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import React from "react";
+import "./SignIn.css";
+import { useHistory } from 'react-router-dom';;;;
+
+function SignIn() {
+   const history = useHistory();
+
+   const handleClick = () => {
+    history.push('/SignUp.jsx');
+  };
+
+  return (
+<>
+     <div className="wave-background">
+      <div className="wave"></div>
+    </div>
+
+
+    <div className="elementoPrincipal">
+
+      <text> Login </text>
+
+      <div className="inputs">
+        <input
+          className="textCaixa"
+          type="text"
+          id="usuario"
+          name="usuario"
+          placeholder="Email, telefone ou usuário"
+          
+         
+        />
+
+        <input
+          className="textCaixa"
+          type="password"
+          id="senha"
+          name="senha"
+          placeholder="Senha"
+         
+         
+        />
+      </div>
+
+
+      <button className="esqueceuSenha"> Esqueceu a senha? </button>
+      {/* adicionar o href pra page de recuperação de senha */}
+
+
+     <input 
+     className="entrarBotao" 
+     type="submit"
+     value="Entrar" />  {/* adicionar o href pra pagina inicial */ } 
+
+      <div className="button-container">
+        <a href="/" className="circle-button">
+          {" "}
+          {/* adicionar a função p entrar com fb */}
+          <img src="./facebook.png" alt="Imagem 1" className="circle-image" />
+        </a>
+
+        <a href="/" className="circle-button">
+          <img src="./google.png" alt="Imagem 2" className="circle-image" />
+        </a>
+      </div>
+
+
+      <button className="botaoCadastro" onClick={handleClick}> Não tenho uma conta </button>{" "}
+      {/* adicionar o href pra pagina de cadastro */}
+
+    </div> </>
+  );
+}
+
+export default SignIn;
